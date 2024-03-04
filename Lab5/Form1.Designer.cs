0f2a718253bf86d50745f51dd415f36670564358
aemendez <aemendez@umes.edu.gt>
2024-03-04 08:59:31 -0600

    Agregar archivos de proyecto.

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewEmpleado = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAsistencia = new System.Windows.Forms.DataGridView();
+            this.buttonleer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEmpleado
+            // 
+            this.dataGridViewEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpleado.Location = new System.Drawing.Point(47, 60);
+            this.dataGridViewEmpleado.Name = "dataGridViewEmpleado";
+            this.dataGridViewEmpleado.RowHeadersWidth = 51;
+            this.dataGridViewEmpleado.RowTemplate.Height = 24;
+            this.dataGridViewEmpleado.Size = new System.Drawing.Size(238, 193);
+            this.dataGridViewEmpleado.TabIndex = 0;
+            // 
+            // dataGridViewAsistencia
+            // 
+            this.dataGridViewAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAsistencia.Location = new System.Drawing.Point(317, 58);
+            this.dataGridViewAsistencia.Name = "dataGridViewAsistencia";
+            this.dataGridViewAsistencia.RowHeadersWidth = 51;
+            this.dataGridViewAsistencia.RowTemplate.Height = 24;
+            this.dataGridViewAsistencia.Size = new System.Drawing.Size(207, 194);
+            this.dataGridViewAsistencia.TabIndex = 1;
+            // 
+            // buttonleer
+            // 
+            this.buttonleer.Location = new System.Drawing.Point(262, 279);
+            this.buttonleer.Name = "buttonleer";
+            this.buttonleer.Size = new System.Drawing.Size(112, 37);
+            this.buttonleer.TabIndex = 2;
+            this.buttonleer.Text = "Cargar datos";
+            this.buttonleer.UseVisualStyleBackColor = true;
+            this.buttonleer.Click += new System.EventHandler(this.buttonleer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonleer);
+            this.Controls.Add(this.dataGridViewAsistencia);
+            this.Controls.Add(this.dataGridViewEmpleado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewEmpleado;
+        private System.Windows.Forms.DataGridView dataGridViewAsistencia;
+        private System.Windows.Forms.Button buttonleer;
+    }
+}
+
---
+++ Lab5/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        List<Empleado> empleados = new List<Empleado>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void  cargar_Empleados()
+        {
+            string fileName = "Empleados.txt";
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+
+            {
+                //leer los datos de un empleado
+                Empleado empleado = new Empleado();
+                empleado.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.SueldoHora = Convert.ToDecimal(reader.ReadLine());
+
+                //guardar el empleado a la lista de empleados
+                empleados.Add(empleado);
+            }
+            reader.Close();
+
+        }
+        public void MO
+        private void buttonleer_Click(object sender, EventArgs e)
+        {
+            //cargar empleados
+            
+
+            
+            //Mostrar la lista de empleados en el Griview
+            dataGridViewEmpleado.DataSource = null;
+            dataGridViewEmpleado.DataSource = empleados;
+            dataGridViewEmpleado.Refresh();
+        }
+    }
+}
